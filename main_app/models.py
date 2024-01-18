c1218949096893d429a1c60bf6dca4ad2e6ed0d2
CosmicGate <jackie.sechrist.3@gmail.com>
2024-01-18 12:33:57 -0600

    Part 2 Completed

--- main_app/models.py
+++ main_app/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Finch(models.Model):
-    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    scientific_name = models.CharField(max_length=100)
+    personality = models.TextField(max_length=150)
+    age_span = models.IntegerField()
 
-
+    def _str_(self):
+        return self.breed
 
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from .models import Finch
 
-finches = [
+# finches = [
 
-    {'breed': 'Zebra Finch', 'scientific_name': 'Taeniopygia guttata', 'personality': 'does not wnat to be handled'},
-    {'breed': 'Gouldian Finch', 'scientific_name' : 'Chloebia Gouldiae', 'personality' : 'Lovely to watch, not to hold'}
+#     {'breed': 'Zebra Finch', 'scientific_name': 'Taeniopygia guttata', 'personality': 'does not wantt to be handled', 'age_span' :'10'},
+#     {'breed': 'Gouldian Finch', 'scientific_name' : 'Chloebia Gouldiae', 'personality' : 'Lovely to watch, not to hold', 'age_span' : '4'}
 
-]
+# ]
 
 # Create your views here.
 
@@ -16,6 +17,11 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all() # retrieve all finches
     return render(request, 'finches/index.html', {
         'finches': finches
     })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {'finch': finch})
