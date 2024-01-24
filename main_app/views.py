e8a98246b9980b41686d86e5872dd98c9c206169
CosmicGate <jackie.sechrist.3@gmail.com>
2024-01-24 17:02:28 -0600

    Part 4 Complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 from .forms import FeedingForm
@@ -40,6 +40,17 @@ def finches_detail(request, finch_id):
     return render (request, 'finches/detail.html', {
        'finch': finch, 'feeding_form': feeding_form
     })
+
+def add_feeding(request, finch_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+    # don't save the form to the db until it
+    # has the finch_id assigned
+        new_feeding = form.save(commit=False)
+        new_feeding.finch_id = finch_id
+        new_feeding.save()
+    return redirect('detail', finch_id=finch_id)
+    
 
 class FinchCreate(CreateView):
   model = Finch
